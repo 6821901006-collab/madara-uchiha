658d1ca2020cb23e90223d04de80687a55a8f6a6
6821901006-collab <6821901006@cdti.ac.th>
2026-06-11 10:29:55 +0700

    'update

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,8 +1,13 @@
 export default function Register() {
   return (
-    <div >
-      <h2> สมัครสมาชิก </h2>
-      <p> โดย อติเมตก์ เติมรุ่งเรืองเลิศ </p>
+    <div  className="auth-page">
+      <form className="auth-card">
+        <h2> สมัครสมาชิก </h2>
+        <input placeholder='Name' type='text'/>
+        <li/>
+        <input placeholder='Email' type="email"/>
+        <button>Register</button>
+      </form>
     </div>
   );
 }
